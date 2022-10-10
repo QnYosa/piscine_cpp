1baefb2adbd5bea452251a455482e4e5a1b36cb7
QnYosa <dimitriyoula@gmail.com>
2022-10-10 23:52:02 +0200

    interface going on

--- Module04/ex03/AMateria.cpp
+++ Module04/ex03/AMateria.cpp
@@ -3,15 +3,29 @@
 
 AMateria::AMateria(/* args */)
 {
+	std::cout << "AMateria default constructor" <<std::endl;
 }
 
 AMateria::~AMateria()
 {
+	std::cout << "AMateria destructor" <<std::endl;
 }
 
-
+AMateria::AMateria(AMateria const & src)
+{
+	std::cout << "AMateria copy constructor" << std::endl;
+	*this = src;
+}
 
 std::string const & AMateria::getType()const
 {
 	return (_type);
+}
+
+AMateria	& AMateria::operator=(AMateria const & src)
+{
+	if (this != &src)
+	{
+		this->_type = src._type;
+	}
 }
