219574954eb8244cfff981317f976ea442100d5c
QnYosa <dimitriyoula@gmail.com>
2022-10-14 19:25:55 +0200

    ex03 done

--- Module04/ex03/main.cpp
+++ Module04/ex03/main.cpp
@@ -5,59 +5,61 @@
 
 int main (void)
 {
-	// AMateria 	**floor;
-	// floor = NULL;
-	// (void)floor;
-	// Character 	*main = new Character("Kratos");
-	// Character 	*nemico = new Character("Thor");
+	AMateria 	**floor;
+	floor = NULL;
+	(void)floor;
+	Character 	*main = new Character("Kratos");
+	Character 	*nemico = new Character("Thor");
 
-	// std::cout << "==================TEST ICE====================" << std::endl << std::endl;
+	std::cout << "==================TEST ICE====================" << std::endl << std::endl;
 
-	// Ice 		*glace = new Ice();
-	// AMateria 	*Aice = glace;
-	// Aice->use(*nemico);
-	// std::cout << Aice->getType() << std::endl;
+	Ice 		*glace = new Ice();
+	AMateria 	*Aice = glace;
+	Aice->use(*nemico);
+	std::cout << Aice->getType() << std::endl;
 
-	// std::cout << "==================TEST CURE====================" << std::endl << std::endl;
+	std::cout << "==================TEST CURE====================" << std::endl << std::endl;
 	
-	// Cure 		*vaccine = new Cure();
-	// Aice = vaccine;
-	// Aice->use(*nemico);
+	Cure 		*vaccine = new Cure();
+	Aice = vaccine;
+	Aice->use(*nemico);
 	
-	// std::cout << "==================TEST AMATERIA====================" << std::endl << std::endl;
-	// Cure		*cure = new Cure ();
-	// std::cout << cure->getType() << std::endl;
-	// AMateria	*Acure = cure;
-	// Aice = Acure;
-	// Aice->use(*nemico);
-	// std::cout << Aice->getType() << "desormais de type cure " << std::endl;
+	std::cout << "==================TEST AMATERIA====================" << std::endl << std::endl;
+	Cure		*cure = new Cure ();
+	std::cout << cure->getType() << std::endl;
+	AMateria	*Acure = cure;
+	Aice = Acure;
+	Aice->use(*nemico);
+	std::cout << Aice->getType() << "desormais de type cure " << std::endl;
 	
-	// std::cout << "==================TEST CHARACTER===================" << std::endl << std::endl;
-	// Character	*cpy = new Character();
-	// std::cout << "Name: " << cpy->getName() << std::endl;
-	// // delete cpy;
-	// cpy =  NULL;
-	// cpy = main;
-	// std::cout << "New Name: " << cpy->getName() << std::endl;
-	// // std::cout << cpy.getName() << std::endl;
-	// main->use(0, *nemico); // test a vide pas de segfault
-	// main->use(3, *nemico); // idem
-	// main->equip(glace);
-	// main->equip(cure);
-	// main->use(1, *nemico);
-	// main->use(3, *nemico);
-	// main->unequip(5); // pas de crash
-	// main->unequip(2); // pas de crash alors qu'on a pas d'arme a cet index
-	// main->use(3, *nemico);
+	std::cout << "==================TEST CHARACTER===================" << std::endl << std::endl;
+	Character	*cpy = new Character();
+	std::cout << "Name: " << cpy->getName() << std::endl;
+	delete cpy;
+	cpy =  NULL;
+	cpy = main;
+	std::cout << "New Name: " << cpy->getName() << std::endl;
+	// std::cout << cpy.getName() << std::endl;
+	main->use(0, *nemico); // test a vide pas de segfault
+	main->use(3, *nemico); // idem
+	main->equip(glace);
+	main->equip(cure);
+	// test a faire envoyer plein de equip
+	main->use(1, *nemico);
+	main->use(3, *nemico);
+	main->unequip(5); // pas de crash
+	main->unequip(2); // pas de crash alors qu'on a pas d'arme a cet index
+	main->use(3, *nemico);
 
-	// std::cout << "==================TEST MATERIASOURCE===================" << std::endl << std::endl;
-	// std::cout << glace->getType() << std::endl;
-	// std::cout << main->getName() << std::endl;
+	std::cout << "==================TEST MATERIASOURCE===================" << std::endl << std::endl;
+	std::cout << glace->getType() << std::endl;
+	std::cout << main->getName() << std::endl;
 	// delete vaccine;
 	// delete glace;
 	// delete cure;
-	// delete main;
-	// delete nemico;
+	delete main;
+	delete nemico;
+	delete vaccine;
 	// delete cpy;
 	// delete glace;
 	// std::string *tab[4];
